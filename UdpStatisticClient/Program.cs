6bd36e3498e3eb5785b68980f3af59cefa761777
Oleh <jupiterfirepower@gmail.com>
2018-07-09 12:55:47 +0300

    async

--- UdpStatisticClient/Program.cs
+++ UdpStatisticClient/Program.cs
@@ -149,30 +149,6 @@ namespace UdpStatisticClient
                 Console.WriteLine("Can't parse config parameter(from config.xml) [timespanmiliseconds] in int. Run with default(500).");
                 Task.Factory.StartNew(() => Receiver(token, multicastAddress, 2222), token);
             }
-
-            /*const int upperBoundary = (int)(Int32.MaxValue * 0.96);
-
-            if (correct)
-            {
-                Action<Task> repeatAction = null;
-
-                repeatAction = _ignored1 =>
-                {
-                    try
-                    {
-                        if (RandomValues.Count >= upperBoundary)
-                            RandomValues.Clear();
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Error: {ex.Message}");
-                    }
-
-                    Task.Delay(new TimeSpan(0, 10, 0), token).ContinueWith(_ignored2 => repeatAction(_ignored2), token); // Repeat every 10 min
-                };
-
-                Task.Delay(5000, token).ContinueWith(repeatAction, token); // Launch with 5 sec delay
-            }*/
         }
 
         static void Main()
